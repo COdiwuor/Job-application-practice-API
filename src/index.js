563228b4d9395c1ced33bb1a2383c448ed53a680
Cyril Owuor <cyrilondime@gmail.com>
2025-11-05 09:57:30 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://jobicy.com/api/v2/remote-jobs?count=20&geo=canada';
+const apiUrl = 'https://jobicy.com/api/v2/remote-jobs?count=20&geo=usa';
 const jobListingsContainer = document.getElementById('job-listings');
 const jobDetailsContainer = document.getElementById('job-details');
 const searchBar = document.getElementById('search-bar');
